2015ff8aee549890d05cc6d1f7d53f4ad75166d4
Tehilah <tkangamba@hotmail.com>
2022-06-03 23:54:52 -0400

    add new address

---
+++ components/AddNewAddress.js
@@ -0,0 +1,30 @@
+import {Button, Modal} from "react-bootstrap";
+
+const AddNewAddress = (props) => {
+    return (
+        <Modal
+            {...props}
+            size="sm"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Modal heading
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className={"d-flex"}>
+                    <input  />
+                    <Button>Add address</Button>
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+
+export default AddNewAddress;
--- components/AddressSetter.js
+++ components/AddressSetter.js
@@ -1,5 +1,5 @@
-import {Dropdown, ListGroup} from "react-bootstrap";
-import { useState, createContext, useContext } from "react";
+import {Button, Dropdown, ListGroup} from "react-bootstrap";
+import {useState, createContext, useContext} from "react";
 import {
     faArrowCircleDown,
     faArrowDown,
@@ -9,53 +9,87 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-function DropDownMenu() {
-    return(
+import AddNewAddress from "./AddNewAddress";
+
+function DropDownMenu(props) {
+    const [addAddressClicked, setAddressClicked] = useState(false)
+    const changeClickStatus = props.changeClickStatus;
+    const newDropStatus = props.newDropStatus;
+    return (
         <>
-            <ListGroup.Item className={"d-flex flex-row"}>
+            <ListGroup.Item className={"d-flex flex-row"} as={Button}
+                            onClick={
+                                () => {
+                                    changeClickStatus(false)
+                                    newDropStatus(true)
+                                }
+                            }
+            >
+
                 Add new address
+
             </ListGroup.Item>
-            <ListGroup.Item className={"d-flex flex-row  justify-content-between"}>
-                <FontAwesomeIcon icon={faMapMarked} />
-                <div style={{lineHeight:"0.2rem"}}>
-                    <p>63rd 10th Street</p>
-                    <p>Chingola,Zambia</p>
-                </div>
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+            <ListGroup.Item>
+
+                <label className={"d-flex flex-row  justify-content-between"} style={{lineHeight: "0.2rem"}}>
+                    <FontAwesomeIcon icon={faMapMarked}/>
+                    <div>
+                        <p>63rd 10th Street</p>
+                        <p>Chingola,Zambia</p>
+                    </div>
+                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+                </label>
+
             </ListGroup.Item>
-            <ListGroup.Item className={"d-flex flex-row  justify-content-between"}>
-                <FontAwesomeIcon icon={faMapMarked} />
-                <div style={{lineHeight:"0.2rem"}}>
-                    <p>63rd 10th Street</p>
-                    <p>Chingola,Zambia</p>
-                </div>
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+            <ListGroup.Item>
+                <label className={"d-flex flex-row  justify-content-between"} style={{lineHeight: "0.2rem"}}>
+                    <FontAwesomeIcon icon={faMapMarked}/>
+                    <div>
+                        <p>63rd 10th Street</p>
+                        <p>Chingola,Zambia</p>
+                    </div>
+                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+                </label>
             </ListGroup.Item>
         </>
     )
 }
 
 const AddressSetter = (props) => {
-    const [addressClicked,setAddressClicked] = useState(false)
-  return (
-   <div className={"d-flex flex-column"}>
+    const [newAddressClicked, setNewAddressClicked] = useState(false)
+    const [addressClicked, setAddressClicked] = useState(false);
+    const changeDropStatus = (status) => setAddressClicked(status);
+    const newDropStatus = (status) => setNewAddressClicked(status)
+    return (
+        <>
+        <div className={"d-flex flex-column"}>
 
-           <a
-           onClick={
-               ()=>{
-                   setAddressClicked(!addressClicked)
-               }
-           }
-           >Address<FontAwesomeIcon icon={faArrowDown}/>
-           </a>
-           <ListGroup className="form-check" style={{position:"absolute",marginTop:"1.3rem",width:"15rem"}}>
-               {addressClicked&& <DropDownMenu />
-               }
-           </ListGroup>
+            <a
+                onClick={
+                    () => {
+                        setAddressClicked(!addressClicked);
+                    }
+                }
+            >Address<FontAwesomeIcon icon={faArrowDown}/>
+            </a>
+
+            <ListGroup className="form-check" style={{position: "absolute", marginTop: "1.3rem", width: "15rem"}}>
+                {addressClicked && <DropDownMenu changeClickStatus ={changeDropStatus} newDropStatus = {newDropStatus} />
+                }
+            </ListGroup>
 
 
-   </div>
-  );
+        </div>
+            <AddNewAddress
+                show={newAddressClicked}
+                onHide={
+                () => {
+                    newDropStatus(false)
+                }
+            }
+            />
+        </>
+    );
 };
 
 export default AddressSetter;
