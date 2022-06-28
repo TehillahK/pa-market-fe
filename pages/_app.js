92478ae90c7512ee1c833f71c9eb67c02163e41e
Tehilah <tkangamba@hotmail.com>
2022-06-28 06:25:33 -0400

    added ssr provider

--- pages/_app.js
+++ pages/_app.js
@@ -11,29 +11,32 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import Head from "next/head";
 import React from 'react';
 import {UserProvider} from '@auth0/nextjs-auth0';
+import {SSRProvider} from "react-bootstrap";
 
 config.autoAddCss = false
 
 function MyApp({Component, pageProps}) {
     return (
-        <UserProvider>
-            <Provider store={store}>
-                <Head>
-                    {/* This ways to add css on global website use css @import property and you also paste Link tag also */}
-                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,300&display=swap"
-                        rel="stylesheet"/>
-                    <link rel="stylesheet" type="text/css" charSet="UTF-8"
-                          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"/>
-                    <link rel="stylesheet" type="text/css"
-                          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"/>
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                </Head>
-                <Component {...pageProps} />
-            </Provider>
-        </UserProvider>
+        <SSRProvider>
+            <UserProvider>
+                <Provider store={store}>
+                    <Head>
+                        {/* This ways to add css on global website use css @import property and you also paste Link tag also */}
+                        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                        <link
+                            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,300&display=swap"
+                            rel="stylesheet"/>
+                        <link rel="stylesheet" type="text/css" charSet="UTF-8"
+                              href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"/>
+                        <link rel="stylesheet" type="text/css"
+                              href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"/>
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    </Head>
+                    <Component {...pageProps} />
+                </Provider>
+            </UserProvider>
+        </SSRProvider>
     );
 }
 
