bb3b0a05d49acde8055224052fa3bb258a735e0a
Tehilah <tkangamba@hotmail.com>
2022-06-28 00:25:43 -0400

    configured two factor authentication

--- components/Checkout.js
+++ components/Checkout.js
@@ -1,10 +1,33 @@
-import {Accordion, Button, Card, ListGroup} from "react-bootstrap";
+import {Accordion, Button, Card, ListGroup, Modal} from "react-bootstrap";
 import {useState} from "react";
 import Image from "next/image";
 import ShoppingCart from "./ShoppingCart";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faMobilePhone, faPhone} from "@fortawesome/free-solid-svg-icons";
 
+function VerificationModal(props) {
+    const link = props.link;
+    return (
+        <Modal
+            {...props}
+            size="sm"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            fullscreen
+            scrollable={false}
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Modal heading
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <iframe  style={{height:"100%",width:"100%",overflow:"hidden"}} src={link} scrolling="no"/>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
 
 
 function CardPayment() {
@@ -59,11 +82,11 @@ function CardPayment() {
 
 function MobileMoney() {
     const [inputs, setInputs] = useState({
-        phoneNum:"",
-        network:"MTN"
+        phoneNum: "",
+        network: "MTN"
     });
-    const [purchaseLink,setPurchaseLink] = useState("")
-
+    const [purchaseLink, setPurchaseLink] = useState("")
+    const [modalShow, setModalShow] = useState(false);
     const handleChange = (event) => {
         setInputs(
             {...inputs, [event.target.name]: event.target.value}
@@ -76,79 +99,86 @@ function MobileMoney() {
         const res = await fetch("/api/services/mobilemoney", {
             method: "POST",
             body: JSON.stringify({
-                network:inputs.network,
+                network: inputs.network,
                 mobileNumber: inputs.phoneNum,
             }),
         })
 
         const data = await res.json()
         let result = JSON.parse(data)
-        if (result.status==="success"){
-           // console.log(result.meta.authorization.redirect)
+        if (result.status === "success") {
+            // console.log(result.meta.authorization.redirect)
             setPurchaseLink(result.meta.authorization.redirect)
+            setModalShow(true)
         }
 
 
     }
-    const textStyle = { width: "100%",
+    const textStyle = {
+        width: "100%",
         padding: "12px 20px",
         margin: "8px 0",
         display: "inline-block",
         border: "1px solid #ccc",
         borderRadius: "4px",
         boxSizing: "border-box",
-        zIndex:"1000"
+        zIndex: "1000"
     }
 
     return (
-        <form className={"d-flex flex-column justify-content-evenly align-content-between"} onSubmit={handleSubmit}>
-            <div className={"d-flex flex-row justify-content-center"} style={{marginBottom: "1rem"}}>
-                <div className={"d-flex flex-column justify-content-between align-items-center"}>
+        <>
+            <form className={"d-flex flex-column justify-content-evenly align-content-between"} onSubmit={handleSubmit}>
+                <div className={"d-flex flex-row justify-content-center"} style={{marginBottom: "1rem"}}>
+                    <div className={"d-flex flex-column justify-content-between align-items-center"}>
 
-                    <label className={"d-flex justify-content-center  mb-3"}>
-                        <Image src={"https://firebasestorage.googleapis.com/v0/b/pamarket-63297.appspot.com/o/MTN-Zambia.png?alt=media&token=a8940938-3c61-4f85-94f0-731a52f877a6"}
+                        <label className={"d-flex justify-content-center  mb-3"}>
+                            <Image
+                                src={"https://firebasestorage.googleapis.com/v0/b/pamarket-63297.appspot.com/o/MTN-Zambia.png?alt=media&token=a8940938-3c61-4f85-94f0-731a52f877a6"}
                                 width={20}
-                               height={20}
-                               objectFit={"cover"}
-                        />  Network
-                        <select  value={inputs.network} onChange={handleChange} name="network">
-                            <option value="MTN">MTN</option>
-                            <option value="Airtel">Airtel</option>
-                            <option value="Zamtel">Zamtel</option>
+                                height={20}
+                                objectFit={"cover"}
+                            /> Network
+                            <select value={inputs.network} onChange={handleChange} name="network">
+                                <option value="MTN">MTN</option>
+                                <option value="Airtel">Airtel</option>
+                                <option value="Zamtel">Zamtel</option>
 
-                        </select>
-                    </label>
-                    <label>
-                        <input
-                            type=""
-                            style={textStyle}
-                            ype="tel" id="phone"
-                            value={inputs.phoneNum}
-                            name="phoneNum"
-                            maxLength={10}
-                            placeholder={"09666666666"}
-                            onChange={handleChange}
+                            </select>
+                        </label>
+                        <label>
+                            <input
+                                type=""
+                                style={textStyle}
+                                ype="tel" id="phone"
+                                value={inputs.phoneNum}
+                                name="phoneNum"
+                                maxLength={10}
+                                placeholder={"09666666666"}
+                                onChange={handleChange}
 
-                        />
-                    </label>
+                            />
+                        </label>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <iframe src={purchaseLink} />
-            </div>
-            <OrderDetails />
-            <input style={{backgroundColor:"black",color:"white"}} type="submit" value={"Pay Now"}/>
-        </form>
+                <OrderDetails/>
+                <input style={{backgroundColor: "black", color: "white"}} type="submit" value={"Pay Now"}/>
+            </form>
+            <VerificationModal  link={purchaseLink}
+                                show={modalShow}
+                                onHide={() => setModalShow(false)}
+
+            />
+        </>
     )
 }
 
 function OrderDetails() {
-    return(
-        <Accordion className={"mb-3"} >
+    return (
+        <Accordion className={"mb-3"}>
             <Accordion.Item eventKey="0">
                 <Accordion.Header>Order Details</Accordion.Header>
                 <Accordion.Body>
-                    <ShoppingCart showFooter={false} />
+                    <ShoppingCart showFooter={false}/>
                 </Accordion.Body>
             </Accordion.Item>
         </Accordion>
@@ -168,6 +198,7 @@ const Checkout = () => {
     }
     const btnNotClicked = {padding: "1rem"}
     return (
+
         <div>
             <Card style={{width: "30rem"}}>
                 <Card.Body>
@@ -206,6 +237,7 @@ const Checkout = () => {
                 </Card.Body>
             </Card>
         </div>
+
     )
 }
 
