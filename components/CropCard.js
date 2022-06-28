1534e53f7c50b139183ce079f8c3459e74b502c6
Tehilah <tkangamba@hotmail.com>
2022-06-28 16:23:15 -0400

    fixed text decoration in web pages

--- components/CropCard.js
+++ components/CropCard.js
@@ -15,7 +15,7 @@ const CropCard = (props) => {
             <a
 
                 className={"d-flex flex-row justify-content-between shadow mb-5 bg-body rounded mx-auto"}
-                style={{height: "10rem",width: "100%",textDecoration:"none"}}
+                style={{height: "10rem",width: "100%",textDecoration:"none",color:"black"}}
                 onClick={
                     () => {
                         setCropShow(true)
